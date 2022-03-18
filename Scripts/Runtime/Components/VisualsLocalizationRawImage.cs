016061e50917c6db8461e0701cd14f70234672c9
Vacuoll <52681127+Vacuoll@users.noreply.github.com>
2022-03-18 11:31:14 +0300

    Update VisualsLocalizationRawImage.cs

--- Scripts/Runtime/Components/VisualsLocalizationRawImage.cs
+++ Scripts/Runtime/Components/VisualsLocalizationRawImage.cs
@@ -17,23 +17,44 @@ namespace Visuals
         {
             rawImageComponent = GetComponent<RawImage>();
         }
-
-        public override void LocalizationLoad()
+        public override void LocalizationChange(int languageIndex, bool changeValues)
         {
             if (!localizationEnable) return;
 
+            localizationKey = GetKeyIndexByName(localizationKeyName);
+
+            List<string> values = GetValuesByKeyIndex(localizationKey);
+
+            if (values != null)
+            {
+                SetValues(values[languageIndex]);
+                if (changeValues)
+                {
+                    LocalizationLoad();
+                }
+            }
+        }
+        public override void LocalizationLoad()
+        {
+            if (!localizationEnable) return;
+            localizationKey = GetKeyIndexByName(localizationKeyName);
             textures.Clear();
 
             for (int i = 0; i < LocalizationStorage.GetLanguages().Count; i++)
             {
-                string fileName = LocalizationStorage.GetLocalization()[localizationCategoryIndex].keys[localizationKey].value[i];
-                string path = Application.streamingAssetsPath + "/" + fileName;
-                if (File.Exists(path))
-                {
-                    byte[] textureBytes = File.ReadAllBytes(path);
-                    Texture2D texture2D = new Texture2D(2, 2);
-                    texture2D.LoadImage(textureBytes);
-                    textures.Add(fileName, texture2D);
+                List<LocalizationStorage.KeyItem> keys = LocalizationStorage.GetLocalization()[localizationCategoryIndex].keys;
+                if(localizationKey >= 0 && localizationKey < keys.Count)
+				{
+                    string fileName = keys[localizationKey].value[i];
+                    string path = Application.streamingAssetsPath + "/" + fileName;
+                   
+                    if (File.Exists(path))
+                    {
+                        byte[] textureBytes = File.ReadAllBytes(path);
+                        Texture2D texture2D = new Texture2D(2, 2);
+                        texture2D.LoadImage(textureBytes);
+                        textures.Add(fileName, texture2D);
+                    }
                 }
             }
         }
@@ -41,6 +62,10 @@ namespace Visuals
         {
             if (rawImageComponent != null)
             {
+                if (textures.Count == 0)
+                {
+                    LocalizationLoad();
+                }
                 if (textures.ContainsKey(value))
                 {
                     rawImageComponent.texture = textures[value];
